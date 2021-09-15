3de94bb0526087259f712974b5248ca4db9039a4
JonathanGehret <jonathan@gehrets.de>
2021-09-15 08:34:45 +0200

    bermanss

--- spatial_penguins.R
+++ spatial_penguins.R
@@ -1,6 +1,7 @@
-library(spatstat)
+library(spatstat) 
 library(onpoint)
-library(raster)
+library(raster) # for loading raster files
+library(maptools) # for using as.im.Rasterlayer()
 
 penguins = read.csv(file = "data/penguins/nest_locations_EPSG3031.csv",sep=",")
 
@@ -48,6 +49,10 @@ flacc = raster(x = "data/penguins/flow_acc_01/flow_acc_01.tif")
 crs(flacc) = CRS('+init=EPSG:3031')
 #plot(flacc, add = T)
 
+flacc_16 = raster(x = "data/penguins/flow_acc_16/flow_acc_16.tif")
+plot(flacc_16)
+
+
 # plotting in one place
 par(mfrow=c(1,3))
 plot(elevation, main = "Elevation")
@@ -57,9 +62,34 @@ plot(penguins_ppp, add = T)
 plot(flacc, main = "Flow accumulation")
 plot(penguins_ppp, add = T)
 
-
+# are the points signifcantly correlated with an abiotic covariate
+# later in programita: cut the studdy area? but isn't it already onyl using the  study area!
+?im
+?as.im
 ?spatstat
 ?berman.test()
 
-# are the points signifcantly correlated with an abiotic covariate
-# later in programita: cut the studdy area? but isn't it already onyl using the  study area!
+delev = disaggregate(elevation, fact = 20)
+plot(delev)
+
+# raster to im for use with berman.test (spatstat)
+flim = as.im.RasterLayer(flacc_16)
+cdrim = as.im.RasterLayer(cdr)
+
+# berman.test for interactions 
+#Tests the goodness-of-fit of a Poisson point process model using methods of Berman (1986).
+
+bt_fl_z1 = berman.test(penguins_ppp,flim, which = "Z1")
+bt_cdr_z1 = berman.test(penguins_ppp,cdrim, which = "Z1")
+bt_fl_z2 = berman.test(penguins_ppp,flim, which = "Z2")
+bt_cdr_z2 = berman.test(penguins_ppp,cdrim, which = "Z2")
+
+bt_cdr
+bt_fl
+
+# plotting of bermant tests
+par(mfrow = c(2,2))
+plot(bt_cdr_z1, xlab = "Cost Distance")
+plot(bt_fl_z1, xlab = "Flow Accumulation")
+plot(bt_cdr_z2, xlab = "Cost Distance")
+plot(bt_fl_z2, xlab = "Flow Accumulation")
